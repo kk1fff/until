bb2e9ca172a9d27f1f03eca810e2a9dd5fc917da
kk1fff <kk1fff@patrickz.net>
2012-12-08 18:20:09 +0800

    Adding package.json and adding test cases.

--- test.js
+++ test.js
@@ -12,10 +12,104 @@
 //   See the License for the specific language governing permissions and
 //   limitations under the License.
 
-var until = require('./lib/main.js'),
+var until  = require('./lib/main.js'),
     events = require('events');
 
-add_test(function test_single_success(tester) {
+function Tester() {
+}
+
+Tester.prototype = {
+  addTest: function addTest(testFunction) {
+    this._testQueue.push(testFunction);
+  },
+
+  run: function run() {
+    this._runNext();
+  },
+
+  is: function is(var1, var2, msg) {
+    if (var1 === var2) {
+      return this.success(msg);
+    }
+
+    if (var1 instanceof Array && var2 instanceof Array) {
+      return this.isTheSameArray(var1, var2, msg);
+    }
+
+    return this.fail("Expected: " + var1 + " == " + var2 + 
+                     (msg ? ": " + msg : ""));
+  },
+
+  isTheSameArray: function isTheSameArray(var1, var2, msg) {
+    if (!var1 instanceof Array) return this.fail(msg);
+    if (!var2 instanceof Array) return this.fail(msg);
+    if (var1.length != var2.length) return this.fail(msg);
+    for (var i = 0; i < var2.length; i++) {
+      if (var1[i] != var2[i]) return this.fail(msg);
+    }
+    return this.success(msg);
+  },
+
+  next: function next() {
+    if (this._timer) {
+      clearTimeout(this._timer);
+      this._timer = null;
+    }
+    this._runNext();
+  },
+
+  success: function success(msg) {
+    var m = "";
+    if (msg) {
+      m = ": " + msg;
+    }
+    console.log("** Success" + m);
+  },
+
+  fail: function fail(msg) {
+    var m = "";
+    if (msg) {
+      m = ": " + msg;
+    }
+    console.log("** Fail" + m);
+  },
+
+  _getRunner: function _getRunner() {
+    if (this._testQueue.length == 0) return null;
+
+    var testFunction = this._testQueue.shift();
+    var self = this;
+    return function() {
+      console.log("== Test start");
+      self._timer = setTimeout(self._timeOutFunc.bind(self), self._timeOut);
+      testFunction(self);
+    }
+  },
+
+  _runNext: function _runNext() {
+    var func = this._getRunner();
+    if (func) {
+      setTimeout(func, 0);
+    } else {
+      console.log("Test finished");
+    }
+  },
+
+  _timeOutFunc: function _timeOutFunc() {
+    this.fail("Time out");
+    this._runNext();
+  },
+
+  _testQueue: [],
+
+  _timeOut: 3000,
+
+  _timer: null
+};
+
+var tester = new Tester();
+
+tester.addTest(function test_single_success(tester) {
   var waitings = new until.Waitings();
   var emitter = new events.EventEmitter();
   waitings.add(emitter, 'event1', 'event2');
@@ -27,15 +121,56 @@ add_test(function test_single_success(tester) {
   emitter.emit('event1');
 });
 
-add_test(function test_single_fail(tester) {
+tester.addTest(function test_single_fail(tester) {
   var waitings = new until.Waitings();
   var emitter = new events.EventEmitter();
   waitings.add(emitter, 'event1', 'event2');
   waitings.on('error', function(list) {
-    console.log('got event: ' + JSON.stringify(list));
     tester.next();
   });
   emitter.emit('event2', new Error('testerr'));
 });
 
-add_test(function test_multiple_success
+tester.addTest(function test_multiple_success(tester) {
+  var waitings = new until.Waitings();
+  var emitter = [new events.EventEmitter(),
+                 new events.EventEmitter()];
+  waitings.add(emitter[0], 'event1', 'event2');
+  waitings.add(emitter[1], 'event3', 'event4');
+  waitings.on('ok', function(list) {
+    tester.is(list.length, 2);
+    tester.next();
+  });
+  emitter[0].emit('event1');
+  emitter[1].emit('event3');
+});
+
+tester.addTest(function test_multiple_error_before_success(tester) {
+  var waitings = new until.Waitings();
+  var emitter = [new events.EventEmitter(),
+                 new events.EventEmitter()];
+  waitings.add(emitter[0], 'event1', 'event2');
+  waitings.add(emitter[1], 'event3', 'event4');
+  waitings.on('error', function(err, list) {
+    tester.is(list.length, 0);
+    tester.next();
+  });
+  emitter[0].emit('event2');
+  emitter[1].emit('event3');
+});
+
+tester.addTest(function test_multiple_error_after_success(tester) {
+  var waitings = new until.Waitings();
+  var emitter = [new events.EventEmitter(),
+                 new events.EventEmitter()];
+  waitings.add(emitter[0], 'event1', 'event2');
+  waitings.add(emitter[1], 'event3', 'event4');
+  waitings.on('error', function(err, list) {
+    tester.is(list.length, 1);
+    tester.next();
+  });
+  emitter[0].emit('event1');
+  emitter[1].emit('event4');
+});
+
+tester.run();
